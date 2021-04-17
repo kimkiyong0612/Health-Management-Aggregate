da22b343e5b1b185271aa7d0b5b5594fea782a4e
KIm <fenderst0612@gmail.com>
2021-04-17 15:39:51 +0900

    add: google_spread_sheet取得

--- main/app.py
+++ main/app.py
@@ -1,42 +1,41 @@
 import json
-
+import gspread
 # import requests
+
+from oauth2client.service_account import ServiceAccountCredentials
+"""local test
+$ sam build && sam local start-api
+"""
+
+
+JSONFILE = "secreat.json"
+SPREAD_SHEET_KEY = "1-NrRiDIs4i-I-Xl9QAvuTzoxLmZxLAsZ3YvrUcSDtnM"
+
+
+# Google Spread Sheetsにアクセス
+def connect_gspread(jsonfile, key):
+    try:
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            jsonfile, scope)
+        gc = gspread.authorize(credentials)
+        SPREADSHEET_KEY = key
+        worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+        return worksheet
+    except Exception as e:
+        print(e)
+
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    ws = connect_gspread(JSONFILE, SPREAD_SHEET_KEY)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    print(ws)
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "get request",
             # "location": ip.text.replace("\n", "")
         }),
     }
